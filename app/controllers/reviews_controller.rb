d05a092dc1a01243c53a6f29ae9a80b9a076b665
Cecile Varin <varincecile@hotmail.com>
2017-11-26 21:42:51 +0100

    mvp yelp ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,20 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
   def create
-    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-
+    # raise
     if @review.save
+      @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render 'restaurant/show'
+      render :new
     end
-
   end
 
   private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
+  RATINGS = (0..5).to_a
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, numericality: true, inclusion: { in: (0..5).to_a,
+  validates :rating, presence: true, numericality: true, inclusion: { in: RATINGS,
             message: "%{value} is not a valid rating" }
 end
 
